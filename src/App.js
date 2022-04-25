14a4391bd00baf9fb6a745d257c1e47e8f2a9397
Catherine <c.allsop32@gmail.com>
2022-04-25 17:12:59 +0100

    formatting

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Search />
     <div className="openSource"> 
   <a href="https://github.com/CatherineAllsop/react-weather-app.git" target="_blank" rel="noreferrer">Open-source code</a> by Catherine Allsop
-</div>
+  </div>
   </div>
   </div>
   );
